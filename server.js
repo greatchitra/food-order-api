4528b5131038c33a48807a49c752a5fa9664a386
greatchitra <gautamchitra276@gmail.com>
2025-08-02 22:52:18 +0530

    Initial commit

--- server.js
+++ server.js
@@ -15,21 +15,27 @@ const app=express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "https://foodie-hub-sigma.vercel.app", 
-    //  origin: "http://localhost:5173", 
+    //origin: "https://foodie-hub-sigma.vercel.app", 
+      origin: "http://localhost:5173", 
      credentials: true, // Allows cookies to be sent
      methods: ["GET", "POST", "PUT", "DELETE", "PATCH"], // Allowed HTTP methods
      allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
  }));
  
+
  app.use((req, res, next) => {
-     res.header("Access-Control-Allow-Origin", "https://foodie-hub-sigma.vercel.app");
+     res.header("Access-Control-Allow-Origin", "http://localhost:5173");
      res.header("Access-Control-Allow-Credentials", "true");
      res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
      res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
      next();
  });
 
+ //app.use("/api", authRouter);
+// after app and middleware setup but before routers
+app.get('/', (req, res) => {
+  res.send('API is working!');
+});
 
 
 app.use('/', authRouter);
@@ -49,6 +55,8 @@ mongoose.connect(process.env.MONGO_URI)
 .catch((err) => console.error(err));
 
 
-app.listen(process.env.PORT, ()=>{
-    console.log('Server is running on port '+process.env.PORT);
-})
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log('Server is running on port ' + PORT);
+});
+
